3a27e53bd9eb69a231f1c8cbf4ad754ebcf4010e
francisco-igor <61854132+francisco-igor@users.noreply.github.com>
2023-02-25 23:40:00 -0300

    Update binary_tree.c

--- Cap13_ArvoresBinarias/Exercicio_01/binary_tree.c
+++ Cap13_ArvoresBinarias/Exercicio_01/binary_tree.c
@@ -1,4 +1,11 @@
-#include "./fila.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct arv {
+  char item;
+  struct arv *esq;
+  struct arv *dir;
+} *Arv;
 
 Arv arv_cria(char n, Arv sae, Arv sad) {
   Arv p = malloc(1*sizeof(struct arv));
@@ -12,7 +19,7 @@ void imprime(Arv a) {
   if (a == NULL) {
     printf("");
   } else {
-    printf("<%d", a -> item);
+    printf("<%c", a -> item);
     imprime(a -> esq);
     imprime(a -> dir);
     printf(">");
@@ -20,15 +27,14 @@ void imprime(Arv a) {
 }
 
 int main() {
-  Arv f = arv_cria(6, NULL, NULL);
-  Arv e = arv_cria(5, NULL, NULL);
-  Arv d = arv_cria(4, NULL, NULL);
-  Arv c = arv_cria(3, NULL, f);
-  Arv b = arv_cria(2, d, e);
-  Arv a = arv_cria(1, b, c);
+  Arv f = arv_cria('F', NULL, NULL);
+  Arv e = arv_cria('E', NULL, NULL);
+  Arv d = arv_cria('D', NULL, NULL);
+  Arv c = arv_cria('C', NULL, f);
+  Arv b = arv_cria('B', d, e);
+  Arv a = arv_cria('A', b, c);
 
   imprime(a);
-  destroif(&a);
   
   return 0;
 }
